1bd409777d4b22f4eb026f93752901178b1e953d
Codega <fcodega2@gmail.com>
2021-03-22 12:05:44 +0100

    small changes

--- dashboard/stock2flow_dashboard.py
+++ dashboard/stock2flow_dashboard.py
@@ -21,8 +21,8 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG],
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}])
 
-app.css.append_css(
-    {"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+# app.css.append_css(
+#     {"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 server = app.server
 
