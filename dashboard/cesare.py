0282bd513dbf2a899b7ff571bdf4b6bd7cb44c61
Codega <fcodega2@gmail.com>
2021-04-16 09:32:04 +0200

    change

--- dashboard/cesare.py
+++ dashboard/cesare.py
@@ -138,4 +138,4 @@ def update_output(mess, key_):
 print("Done")
 # --------------------
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8000, host='0.0.0.0')
+    app.run_server(debug=False, port=9000, host='0.0.0.0')
