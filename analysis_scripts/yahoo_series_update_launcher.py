85fd4163d368529df4e5fc9675b97e0495177c8e
Codega <fcodega2@gmail.com>
2021-05-20 10:32:43 +0200

    logging added

--- analysis_scripts/yahoo_series_update_launcher.py
+++ analysis_scripts/yahoo_series_update_launcher.py
@@ -1,20 +1,20 @@
-from btc_analysis.config import (NAME_FOR_MKT_CAP, TICKERS_FOR_MKT_CAP,
-                                 YAHOO_TO_DOWNLOAD_CODE,
+from btc_analysis.config import (YAHOO_TO_DOWNLOAD_CODE,
                                  YAHOO_TO_DOWNLOAD_NAME, YAHOO_TO_RETURN)
-from btc_analysis.market_data import (mkt_cap_op,
-                                      mkt_data_op,
-                                      yesterday_str,
-                                      index_coll_check
-                                      )
-from btc_analysis.mongo_func import (mongo_coll_drop, mongo_indexing,
-                                     )
+from btc_analysis.market_data import mkt_data_op, yesterday_str
+from btc_analysis.mongo_func import mongo_coll_drop, mongo_indexing
+import logging
 
+# logging configuration
+logging.basicConfig(filename='log_file.log', filemode='w',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# mongo collections indexing
 mongo_indexing()
 
-
-# index_coll_check()
 # ---------------------------------------------
 # yahoo series update
+
+logging.info('yahoo_series_update_launcher.py start')
 
 mongo_coll_drop("yahoo")
 
@@ -22,3 +22,5 @@ yesterday_str = yesterday_str()
 
 mkt_data_op(YAHOO_TO_DOWNLOAD_CODE, YAHOO_TO_DOWNLOAD_NAME,
             YAHOO_TO_RETURN, "2015-12-31", yesterday_str)
+
+logging.info('yahoo_series_update_launcher.py end')
