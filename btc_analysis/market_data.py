7f8e39ead0e6d86b47424934b5e5d96e48b85ab4
Codega <fcodega2@gmail.com>
2021-07-19 14:19:42 +0200

    testing

--- btc_analysis/market_data.py
+++ btc_analysis/market_data.py
@@ -703,14 +703,15 @@ def check_missing_days(coll_to_look, type_="other"):
 
     last_day = df_hist.tail(1)
     last_date = np.array(last_day["Date"])[0]
-    print(last_date)
+    print(type(last_date))
+    print(type(yesterday))
     list_of_days = date_gen(last_date, yesterday)
     print(list_of_days)
     list_of_days.pop(0)
 
     return list_of_days
 
-    
+
 # ----------------------
 # HISTORICAL VOLATILITY
 # ----------------------
