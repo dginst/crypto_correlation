74d503e2b29c98e102ed8faedb24645571ec6945
Codega <fcodega2@gmail.com>
2021-07-19 12:08:17 +0200

    logging added to btc_net_daily script

--- analysis_scripts/btc_net_daily.py
+++ analysis_scripts/btc_net_daily.py
@@ -1,3 +1,4 @@
+import logging
 import pandas as pd
 import numpy as np
 
@@ -7,7 +8,14 @@ from btc_analysis.market_data import (
     btc_supply_op, check_and_add_daily, blockchain_stats_op, yesterday_str,
     check_missing_days)
 
+# logging configuration
+logging.basicConfig(filename='log_file.log', filemode='a',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
+logging.info('btc_net_daily.py start')
+
+# --
 mongo_coll_drop("supply")
 mongo_coll_drop("btc_network")
 
@@ -19,77 +27,116 @@ yesterday_ = yesterday_str("%d-%m-%Y")
 # ---
 # btc price collection update
 
-crypto_price_df = query_mongo("index", "crypto_price")
-btc_tot_df = crypto_price_df[["BTC"]]
+try:
 
-list_of_missing = check_missing_days("btc_price", type_="price")
-lenght_missing = len(list_of_missing)
+    crypto_price_df = query_mongo("index", "crypto_price")
+    btc_tot_df = crypto_price_df[["BTC"]]
 
-if lenght_missing > 1:
+    list_of_missing = check_missing_days("btc_price", type_="price")
+    lenght_missing = len(list_of_missing)
 
-    btc_missing = np.array(btc_tot_df.tail(lenght_missing))
-    new_arr = np.column_stack((list_of_missing, btc_missing))
-    new_df = pd.DataFrame(new_arr, columns=["Date", "BTC Price"])
-    new_df["BTC Price"] = [float(x) for x in new_df["BTC Price"]]
-    mongo_upload(new_df, "collection_btc_price")
+    if lenght_missing > 1:
 
-elif lenght_missing == 1:
+        btc_missing = np.array(btc_tot_df.tail(lenght_missing))
+        new_arr = np.column_stack((list_of_missing, btc_missing))
+        new_df = pd.DataFrame(new_arr, columns=["Date", "BTC Price"])
+        new_df["BTC Price"] = [float(x) for x in new_df["BTC Price"]]
+        mongo_upload(new_df, "collection_btc_price")
 
-    btc_last = np.array(btc_tot_df.tail(1))[0]
-    new_arr = np.column_stack((yesterday_, btc_last))
-    new_df = pd.DataFrame(new_arr, columns=["Date", "BTC Price"])
-    new_df["BTC Price"] = [float(x) for x in new_df["BTC Price"]]
-    mongo_upload(new_df, "collection_btc_price")
+    elif lenght_missing == 1:
 
-elif lenght_missing == 0:
+        btc_last = np.array(btc_tot_df.tail(1))[0]
+        new_arr = np.column_stack((yesterday_, btc_last))
+        new_df = pd.DataFrame(new_arr, columns=["Date", "BTC Price"])
+        new_df["BTC Price"] = [float(x) for x in new_df["BTC Price"]]
+        mongo_upload(new_df, "collection_btc_price")
 
+    elif lenght_missing == 0:
+
+        pass
+
+
+except Exception:
+
+    logging.error("Exception occurred", exc_info=True)
+    logging.info(
+        'BTC prices collection failed to update')
     pass
 
 # ---
 # daily blockchain info
 
-blockchain_stats_op()
+try:
 
-daily_df = query_mongo("btc_analysis", "btc_network")
+    blockchain_stats_op()
 
+    daily_df = query_mongo("btc_analysis", "btc_network")
+
+except Exception:
+
+    logging.error("Exception occurred", exc_info=True)
+    logging.info(
+        'BTC network operation failed to update')
+    pass
 # ---
 # hash rate
 
-hr_df = daily_df.copy()
-hr_df = hr_df[["Hash Rate"]]
-hr_df["Date"] = yesterday
-hr_df["Hash Rate"] = [float(x) for x in hr_df["Hash Rate"]]
+try:
 
-check_and_add_daily(hr_df, "hash_rate", "collection_hash_rate")
+    hr_df = daily_df.copy()
+    hr_df = hr_df[["Hash Rate"]]
+    hr_df["Date"] = yesterday
+    hr_df["Hash Rate"] = [float(x) for x in hr_df["Hash Rate"]]
+
+    check_and_add_daily(hr_df, "hash_rate", "collection_hash_rate")
+
+except Exception:
+
+    logging.error("Exception occurred", exc_info=True)
+    logging.info(
+        'BTC hash rate collection failed to update')
+    pass
 
 # ----
 # supply
 
-supply_df = daily_df.copy()
-supply_df = supply_df[["Daily BTC", "Daily Block"]]
+try:
 
-new_btc = np.array(supply_df["Daily BTC"])[0]
-new_block = np.array(supply_df["Daily Block"])[0]
-new_supply_arr = np.column_stack((yesterday, new_btc, new_block))
-new_supply_df = pd.DataFrame(new_supply_arr, columns=[
-                             "Date", "BTC Issuance", "BTC Blocks"])
+    supply_df = daily_df.copy()
+    supply_df = supply_df[["Daily BTC", "Daily Block"]]
 
-new_supply_df["BTC Issuance"] = [float(x)
-                                 for x in new_supply_df["BTC Issuance"]]
-new_supply_df["BTC Blocks"] = [float(x) for x in new_supply_df["BTC Blocks"]]
+    new_btc = np.array(supply_df["Daily BTC"])[0]
+    new_block = np.array(supply_df["Daily Block"])[0]
+    new_supply_arr = np.column_stack((yesterday, new_btc, new_block))
+    new_supply_df = pd.DataFrame(new_supply_arr, columns=[
+        "Date", "BTC Issuance", "BTC Blocks"])
 
+    new_supply_df["BTC Issuance"] = [float(x)
+                                     for x in new_supply_df["BTC Issuance"]]
+    new_supply_df["BTC Blocks"] = [float(x)
+                                   for x in new_supply_df["BTC Blocks"]]
 
-# updating Block Number and BTC Issuance
-check_and_add_daily(new_supply_df, "btc_hist_supply",
-                    "collection_hist_supply")
+    # updating Block Number and BTC Issuance
+    check_and_add_daily(new_supply_df, "btc_hist_supply",
+                        "collection_hist_supply")
 
-# update supply computation and upload
+    # update supply computation and upload
 
-initial_supply_df = query_mongo("btc_analysis", "btc_hist_supply")
+    initial_supply_df = query_mongo("btc_analysis", "btc_hist_supply")
 
-final_supply_df = btc_supply_op(initial_supply_df)
+    final_supply_df = btc_supply_op(initial_supply_df)
 
-mongo_upload(final_supply_df, "collection_total_supply")
+    mongo_upload(final_supply_df, "collection_total_supply")
 
+except Exception:
+
+    logging.error("Exception occurred", exc_info=True)
+    logging.info(
+        'BTC supply collection failed to update')
+    pass
 # ----
 # difficulty
+
+
+# ----
+logging.info('btc_net_daily.py end')
