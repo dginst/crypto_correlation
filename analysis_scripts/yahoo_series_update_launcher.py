ff8dbcd5438a02254683447f0590cfd2e1c5ae9b
fcodega <fcodega2@gmail.com>
2022-06-07 16:52:58 +0200

    retunrs for crypto

--- analysis_scripts/yahoo_series_update_launcher.py
+++ analysis_scripts/yahoo_series_update_launcher.py
@@ -1,6 +1,6 @@
 import logging
 
-from btc_analysis.config import (YAHOO_TO_DOWNLOAD_CODE,
+from btc_analysis.config import (ASSET_LIST, CRYPTO_LIST, YAHOO_TO_DOWNLOAD_CODE,
                                  YAHOO_TO_DOWNLOAD_NAME, YAHOO_TO_RETURN)
 from btc_analysis.market_data import mkt_data_op, yesterday_str
 from btc_analysis.mongo_func import mongo_coll_drop, mongo_indexing
@@ -21,8 +21,10 @@ mongo_indexing()
 
 yesterday_str_ = yesterday_str()
 
+list_to_return = CRYPTO_LIST + ASSET_LIST
+
 mkt_data_op(YAHOO_TO_DOWNLOAD_CODE, YAHOO_TO_DOWNLOAD_NAME,
-            YAHOO_TO_RETURN, "2015-12-31", yesterday_str_)
+            list_to_return, "2015-12-31", yesterday_str_)
 
 
 logging.info('yahoo_series_update_launcher.py end')
