321618feff4009c640ef8cb7affdb51158b5c807
fcodega <fcodega2@gmail.com>
2023-07-02 15:40:26 +0200

    bug fix

--- analysis_scripts/series_volatility_launcher.py
+++ analysis_scripts/series_volatility_launcher.py
@@ -23,13 +23,13 @@ logret_df = query_mongo(DB_NAME, "all_logreturns_y")
 hist_vola_252 = historical_vola(return_df, logret_df, 252)
 hist_vola_90 = historical_vola(return_df, logret_df, 90)
 hist_vola_30 = historical_vola(return_df, logret_df, 30)
-ewma_vola = decay_volatility(return_df, logret_df, 252)
-print(ewma_vola)
+# ewma_vola = decay_volatility(return_df, logret_df, 252)
+# print(ewma_vola)
 
 mongo_upload(hist_vola_252, "collection_volatility_252")
 mongo_upload(hist_vola_90, "collection_volatility_90")
 mongo_upload(hist_vola_30, "collection_volatility_30")
-mongo_upload(ewma_vola, "collection_volatility_ewm")
+# mongo_upload(ewma_vola, "collection_volatility_ewm")
 
 
 # --------
